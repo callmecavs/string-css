b10668826686c77429046603cbf2ccbfde651e64
Michael Cavalea <callmecavs@gmail.com>
2017-06-10 16:05:20 -0400

    add working concat function

--- src/css.js
+++ src/css.js
@@ -1,6 +1,24 @@
 const css = (strings, ...values) => {
-  console.log('strings: ', strings)
-  console.log('values: ', values)
+  // immutably weave strings and values together
+  const concat = (strings, values) => {
+    const len = strings.length
+
+    let index
+    let result = ''
+
+    for (index = 0; index < len; index++) {
+      result += strings[index]
+
+      // catch one less value than string
+      if (index !== len - 1) {
+        result += values[index]
+      }
+    }
+
+    return result
+  }
+
+  console.log(concat(strings, values))
 }
 
 export default css
